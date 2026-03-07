7dfd1918c15a20c670b0899dd6654fe0404ee749
sumer0709 <sumerrath@gmail.com>
2026-03-07 10:06:38 +0530

    cahnges in db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,11 +3,19 @@ require('dotenv').config();
 const logger = require('../utils/logger');
 
 async function connectDb(){
+const mongoUri = process.env.MONGODB_URI;
+    if (!mongoUri) {
+        const error = new Error('MONGODB_URI is not configured');
+        logger.error(error.message);
+        throw error;
+    }
+
     try{
-        await mongoose.connect(process.env.MONGODB_URI);
+        await mongoose.connect(mongoUri);
         logger.info('Connected to MongoDB');
     }catch(error){
         logger.error('Error connecting to MongoDB', error);
+throw error;
     }
 }
 module.exports = connectDb;
